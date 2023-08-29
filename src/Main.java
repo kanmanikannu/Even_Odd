81c7037f6ad477ab9bb7933e3285f504aa5e7d45
kanmanikannu <kanmanikannu117@gmail.com>
2023-08-29 20:56:17 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number= scan.nextInt();
+        System.out.println(even_odd(number));
+    }
+    static String even_odd(int number)
+    {
+        if (number%2==0)
+        {
+            return "The Number is even";
+        }
+        else
+        {
+            return "The Number is odd";
+        }
+    }
+}
